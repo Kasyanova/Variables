69cdde60f20b8d56ce22c9b3e2b9b23307718b7e
KKasyanova <eliseeva.kseniya@gmail.com>
2022-10-09 14:44:23 +0500

    задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double a = 5.5555;
+        byte b = 127;
+        System.out.printf("1. %f%n",a);
+        System.out.printf("2. %d",b);
     }
 }
